af2c858331e123629ac9fb86a937f903f134ab6f
しむらようすけ <yzz0427@vip.qq.com>
2016-10-15 08:00:36 +0800

    Initial Commit

---
+++ DSHomework4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSHomework4
+//
+//  Created by apple on 16/10/15.
+//  Copyright © 2016年 YangZhiZhuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
